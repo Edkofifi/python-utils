91eda29e7d68342ab9b48baddcf4a829f7d89d63
Edmond Agbovi-Teye <atk.eddy@gmail.com>
2025-11-27 09:49:59 +0000

    generate barcode

---
+++ qr_code/generate_code.py
@@ -0,0 +1,16 @@
+import qrcode
+
+link = ""
+
+qr = qrcode.QRCode(
+    version=2,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(link)
+qr.make(fit=True)
+
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qrcode.png")  
